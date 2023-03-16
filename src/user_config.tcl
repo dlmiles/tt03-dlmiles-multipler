47b24a787c374a31c3fecc3e8e03ed174a72be7e
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-16 18:15:32 +0000

    yosys: user_config.tcl

--- src/user_config.tcl
+++ src/user_config.tcl
@@ -3,34 +3,39 @@
 #
 parray ::env
 
-puts [::pwd]
+puts "PWD [::pwd]"
 
 #
-# The src/Makefile build everything relative to the src dir.
+# The src/Makefile builds everything relative to the src dir.
 # So `include "config.vh" works from a sub-dir *.v, when the file is in src/
 #
 # But OpenLANE appears to manage Verilog `include to means the source is the
 #  location of the verilog file itself.  Or at least does not automatically
-#  include the top level.
+#  find *.vh files at top level.
 #
 
 # https://openlane.readthedocs.io/en/latest/reference/configuration.html#synthesis
 # I assume this can be colon-character ":" delimited like $PATH, the docs are not
 # immediately helpful to know that.
 
-# If a directory "include" exists, then it assumes you strcture everything from there.
-if { [file isdirectory "$::env(DESIGN_DIR)/include"] } {
-  if { [info exists ::env(VERILOG_INCLUDE_DIRS)] } {
-    set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)/include:$::env(VERILOG_INCLUDE_DIRS)"
+# If a directory "include" exists, then it assumes you structure everything from there.
+if { [info exists ::env(DESIGN_DIR)] } {
+  if { [file isdirectory "$::env(DESIGN_DIR)/include"] } {
+    if { [info exists ::env(VERILOG_INCLUDE_DIRS)] } {
+      set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)/include:$::env(VERILOG_INCLUDE_DIRS)"
+    } else {
+      # Don't create an empty path entry
+      set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)"
+    }
   } else {
-    set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)"
-  }
-} else {
-  # Otherwise add in the DESIGN_DIR so it works like icarus/cocotb does locally
-  if { [info exists ::env(VERILOG_INCLUDE_DIRS)] } {
-    set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR):$::env(VERILOG_INCLUDE_DIRS)"
-  } else {
-    set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)"
+    # Otherwise add in the DESIGN_DIR itself so it works like icarus/cocotb does locally
+    # It is assumed DESIGN_DIR = gitproject/src at this time until this script is debugged/checked
+    if { [info exists ::env(VERILOG_INCLUDE_DIRS)] } {
+      set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR):$::env(VERILOG_INCLUDE_DIRS)"
+    } else {
+      # Don't create an empty path entry
+      set ::env(VERILOG_INCLUDE_DIRS) "$::env(DESIGN_DIR)"
+    }
   }
 }
 
