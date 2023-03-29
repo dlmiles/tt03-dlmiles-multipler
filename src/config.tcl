58d70de7665b06e8d9c465d3ef813ff32455ef9b
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-03-29 22:22:09 +0100

    config.tcl: CLOCK_PERIOD=20 50MHz

--- src/config.tcl
+++ src/config.tcl
@@ -61,7 +61,9 @@ set ::env(CLOCK_TREE_SYNTH) 1
 # period is in ns, so 50ns == 20MHz
 #set ::env(CLOCK_PERIOD) "50"
 # period is in ns, so 25ns == 40MHz
-set ::env(CLOCK_PERIOD) "25"
+#set ::env(CLOCK_PERIOD) "25"
+# period is in ns, so 20ns == 50MHz
+set ::env(CLOCK_PERIOD) "20"
 set ::env(CLOCK_PORT) {io_in[0]}
 
 # hold/slack margin
